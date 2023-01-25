c5fe4eff4ed45ee342a31b74e4347c4fd615d162
Carlos <czelayabaca@gmail.com>
2023-01-24 19:10:02 -0600

    Finish: falta publicarla

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { ThemeProvider } from "react-native-rapi-ui";
 import Navigation from "./src/routes/config";
 import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+
+// console.disableYellowBox = true;
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
